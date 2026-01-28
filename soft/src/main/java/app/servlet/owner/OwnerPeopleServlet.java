e0e868c4f0806401bcc84ef8611153d7e7c2b9a8
yasu5727 <270308t@ugs.kochi-tech.ac.jp>
2026-01-28 17:12:06 +0900

    リンク検索で動くように調整しました（接続できなくなったらごめんなさい）

--- soft/src/main/java/app/servlet/owner/OwnerPeopleServlet.java
+++ soft/src/main/java/app/servlet/owner/OwnerPeopleServlet.java
@@ -26,7 +26,7 @@ public class OwnerPeopleServlet extends HttpServlet {
             resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
             resp.setHeader("Pragma", "no-cache");
             resp.setHeader("Expires", "0");
-            resp.sendRedirect(req.getContextPath() + "/login.jsp");
+            resp.sendRedirect(req.getContextPath() + "/LoginServlet");
             return;
         }
 
